d547510b123fe07aa6da96e4cd7b5bc7d81bb4ef
ZOZO-Hacker <kolin.antonin@norskakladno.cz>
2025-03-13 15:34:49 +0000

    sus

--- program.py
+++ program.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>pocuvaj nejdě mi internet za čo ja ti platim ty geno!</p>"
+
+
+@app.route("/huh")
+def hevy():
+    return "<h1>pocuvaj už mi dě internet za čo ja ti platim ty geno!</h1>"
